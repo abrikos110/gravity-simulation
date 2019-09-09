a65c671443c8494415bc4231c7bcfdd42aa3b76b
abrikos110 <=>
2019-09-09 13:26:54 +0300

    Remove some useless files

--- cr.bat
+++ cr.bat
@@ -1,3 +1,4 @@
 g++ .\code\SSP.cpp -I.\libs\include -L.\libs\lib -w    -lmingw32 -lSDL2main -lSDL2 -lfreeglut -lglu32 -lopengl32 -o .\SSP2.exe -std=c++11
-dontdoanything		+	
-dontdoanything		here -Wl,-subsystem,windows if want without console
+
+# +	
+# here -Wl,-subsystem,windows if want without console
